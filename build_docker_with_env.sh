88c5bce7c1825bb92aa904a20a7e4d4cfd60bf97
Nathan T <nathan0812@gmail.com>
2025-10-22 00:43:15 +0300

    .

--- build_docker_with_env.sh
+++ build_docker_with_env.sh
@@ -20,11 +20,12 @@ docker build -f docker/Dockerfile \
     --build-arg nvcc_threads=1 --build-arg max_jobs=1 \
     --build-arg USE_SCCACHE=1 \
     --build-arg BUILD_PYTORCH_FROM_SOURCE=1 \
-    --build-arg PYTORCH_VERSION=v2.8.0 \
+    --build-arg BUILD_FLASHINFER_FROM_SOURCE=1 \
+    --build-arg PYTORCH_VERSION=2.8.0 \
     --secret id=sccache_s3_no_credentials,env=SCCACHE_S3_NO_CREDENTIALS \
     --secret id=aws_access_key_id,env=AWS_ACCESS_KEY_ID \
     --secret id=aws_secret_access_key,env=AWS_SECRET_ACCESS_KEY \
     --secret id=sccache_s3_use_ssl,env=SCCACHE_S3_USE_SSL \
     --secret id=sccache_endpoint,env=SCCACHE_ENDPOINT \
     --target vllm-openai \
-    -t vllm/vllm-openai:v0.10.2-aot .
+    -t vllm/vllm-openai:v0.11.0-aot .
